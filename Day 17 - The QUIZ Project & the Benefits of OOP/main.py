73a9f26f073e143effaba6f57e24bdf27603c541
acamara19 <acamara19@gmail.com>
2024-04-16 19:44:59 -0400

    Day 17 - The QUIZ Project & the Benefits of OOP

---
+++ Day 17 - The QUIZ Project & the Benefits of OOP/main.py
@@ -0,0 +1,20 @@
+from data import question_data
+from question_model import Questions
+from quiz_brain import QuizBrain
+
+
+question_bank = []
+for question in question_data:
+    question_text = question['text']
+    question_answer = question['answer']
+    new_question = Questions(question_text, question_answer)
+    question_bank.append(new_question)
+
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_question():
+    quiz.next_question()
+
+print("Congratulations. You've completed the entire quiz!")
+print(f"You're final score: {quiz.score}/{quiz.question_number}")
